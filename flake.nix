694753c2ec7ae5927351e95a0fd3616601f5479f
Keith King <KingKeithC@gmail.com>
2026-04-01 19:14:15 -0600

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A Bot to Bridge Discord Events with Calendars";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            # General development tools
+            git
+
+            # Rust Dev Tools
+            cargo
+            rustc
+          ];
+
+          shellHook = ''
+            echo 'Ready...';
+          '';
+        };
+    };
+}
